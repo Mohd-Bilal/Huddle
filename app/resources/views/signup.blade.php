a74def17ec45d14f18d1cf81cf7d5d4ffb5d0c5d
Mohd-Bilal <bilaljaleel123@gmail.com>
2018-04-07 00:54:06 +0530

    chdlm

--- app/resources/views/signup.blade.php
+++ app/resources/views/signup.blade.php
@@ -10,13 +10,16 @@
        SIGNUP 
     </h1>
     <form action="{{route('postsignup')}}" method="post">
-      First Name   <input type="text" name="fname"><br><br>
-      Last Name<input type="text" name="lname"><br><br>
-      Email<input type="text" name="email"><br><br>
-      Password<input type="password" name="password"><br><br>
-      Confirm Password<input type="password" name="cpassword"><br><br>
-      Date of Birth<input type="text" name="dob"><br><br>
-      Mobile No<input type="text" name="Mobile"><br><br>
+      <label>First Name</label>   <input type="text" name="fname"><br><br>
+      <label>Last Name</label><input type="text" name="lname"><br><br>
+      <label>Email</label><input type="text" name="email"><br><br>
+      @if($Error == "Wrong Password")
+      <div>Wrong Password</div>
+      @endif
+      <label>Password</label><input type="password" name="password"><br><br>
+      <label>Confirm Password</label><input type="password" name="cpassword"><br><br>
+      <label>Date of Birth</label><input type="text" name="dob"><br><br>
+      <label>Mobile No</label><input type="text" name="Mobile"><br><br>
     <input type='hidden' name='_token' value='{{Session::token()}}'>
 
         <button type="Submit">Submit</button>
